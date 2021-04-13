43237f8112d4da25d8a5c4f74035d4e4aa7bb308
Babitababy <kumaribabita2402@gmail.com>
2021-04-13 12:13:24 +0530

    Animation added

--- app/src/main/java/com/example/blinkingtextview/MainActivity.java
+++ app/src/main/java/com/example/blinkingtextview/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.blinkingtextview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.blinkTextView);
     }
 
+    @SuppressLint("WrongConstant")
     public void action_button(View view) {
+        ObjectAnimator animator = ObjectAnimator.ofInt(textView,"backgroundColor", Color.RED,Color.GREEN,Color.BLUE);
+        //time duration for switching between colors
+        animator.setDuration(500);
+        animator.setEvaluator(new ArgbEvaluator());
+        animator.setRepeatMode(Animation.REVERSE);
+        animator.setRepeatCount(Animation.INFINITE);
+        animator.start();
+
     }
 }
